0978adea9b493f3a0a2b7c496a7e8fd1a632a225
Chovhan <dema200043@gmail.com>
2019-11-14 20:36:07 +0200

    Labwork 5, full site

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.gallery-container__main-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    centerPadding: "0px",
+    dots: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          dots: false
+        }
+      },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        dots: false
+      }
+    }
+    ]
+  });
+});
